d6d3e70021e8ba83516763f6109ab0677eceec8d
senisioi <sergiu.nisioi@gmail.com>
2017-04-22 10:17:15 -0400

    updating scripts to be executable and configurable beam size

--- src/scripts/translate.sh
+++ src/scripts/translate.sh
@@ -5,11 +5,13 @@ RES_DIR=`readlink -f ../../results_${CUR_EXP}`
 mkdir -p $RES_DIR
 MODEL_PATH=`readlink -f ../../models/NTS_epoch11_10.19.t7`
 MODEL=${MODEL_PATH##*/}
-OUTPUT=${RES_DIR}/result_${MODEL}
-LOG_OUT=${RES_DIR}/result_${MODEL}.log
+
+BEAM_SIZE=5
 #GPUS=1,2
 GPUS=0
-BEAM_SIZE=12
+OUTPUT=${RES_DIR}/result_${MODEL}_${BEAM_SIZE}
+LOG_OUT=${RES_DIR}/result_${MODEL}_${BEAM_SIZE}.log
+
 SRC=${DIRECTORY}/test.en
 TGT=${DIRECTORY}/test.sen
 cd $OPENNMT_PATH 
